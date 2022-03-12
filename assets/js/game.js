0d7fda77e16ddce15c6a9c3e93494b06e7ca971d
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-03-11 21:06:49 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,15 @@ var fight = function(enemyName) {
   if (confirmSkip) {
     window.alert(playerName + " has decided to leave the fight. Goodbye!");
     // subtract money from playerMoney for skipping
-    playerMoney = playerMoney - 10;
+    playerMoney = Math.max(0, playerMoney - 10);
     console.log("playerMoney", playerMoney);
     break;
   } 
 }
 
-   // remove enemy's health by subtracting the amount set in the playerAttack variable
-   enemyHealth = enemyHealth - playerAttack;
+   var damage = randomNumber(playerAttack - 3, playerAttack);
+   enemyHealth = Math.max(0, enemyHealth - damage);
+
    console.log(
      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
      );
@@ -42,8 +43,9 @@ if (enemyHealth <=0) {
 } else {
   window.alert (enemyName + " still has " + enemyHealth + " health left. ");
 }
- // remove player's health by subtracting the amount set in the enemyAttack variable
- playerHealth = playerHealth - enemyAttack;
+ var damage = randomNumber(enemyAttack - 3, enemyAttack);
+ playerHealth = Math.max(0, playerHealth - damage);
+
  console.log(
    enemyName + " attacked  " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
  );
@@ -70,7 +72,7 @@ for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
@@ -90,6 +92,11 @@ for (var i = 0; i < enemyNames.length; i++) {
   }
 }
 endGame();
+};
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (min - max + 1) + min);
+
+  return value;
 };
 //function to end the entire game
 var endGame = function() {
